4fe1f31c8795a1b973e904bfef51e3dd56440382
MrTew <88481749+mrtew@users.noreply.github.com>
2025-03-28 17:57:17 +0800

    feat: show user details in me screen

--- lib/views/me/me_screen.dart
+++ lib/views/me/me_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import '../../providers/user_provider.dart';
 import '../../widgets/bottom_nav_bar.dart';
 import '../../widgets/app_bar_widget.dart';
 
@@ -14,92 +15,178 @@ class MeScreen extends ConsumerWidget {
         title: 'Me',
         showBackButton: false,
       ),
-      body: SafeArea(
-        child: ListView(
-          children: [
-            // Profile Section
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                children: [
-                  // Profile Image
-                  Container(
-                    width: 60,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      image: const DecorationImage(
-                        image: AssetImage('assets/images/default_avatar_1024x1024.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  // Profile Info
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+      body: ref.watch(currentUserProvider).when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stackTrace) => Center(child: Text('Error: $error')),
+        data: (user) {  
+          return SafeArea(
+            child: ListView(
+              children: [
+                Divider(
+                  height: 1,
+                  thickness: 1,
+                  indent: 16,
+                  endIndent: 16,
+                  color: Colors.grey[200],
+                ),
+                // Profile Section
+                GestureDetector(
+                  onTap: () {
+                    GoRouter.of(context).push('/profile');
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.white,
+                    child: Row(
                       children: [
-                        const Text(
-                          'Y',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                        // Profile Image
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: user?.avatar != ''
+                              ? Image.network(
+                                  user?.avatar ?? '',
+                                  width: 60,
+                                  height: 60,
+                                  fit: BoxFit.cover,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return Container(
+                                      width: 60,
+                                      height: 60,
+                                      color: Colors.grey[200],
+                                      child: Icon(Icons.person, color: Colors.grey[400], size: 40),
+                                    );
+                                  },
+                                )
+                              : Container(
+                                  width: 60,
+                                  height: 60,
+                                  color: Colors.grey[200],
+                                  child: Icon(Icons.person, color: Colors.grey[400], size: 40),
+                                )
+                        ),
+                        // const SizedBox(width: 8),
+                        // Container(
+                        //   width: 60,
+                        //   height: 60,
+                        //   decoration: BoxDecoration(
+                        //     borderRadius: BorderRadius.circular(8),
+                        //     image: const DecorationImage(
+                        //       image: AssetImage('assets/images/default_avatar_1024x1024.png'),
+                        //       fit: BoxFit.cover,
+                        //     ),
+                        //   ),
+                        // ),
+                        const SizedBox(width: 20),
+                        // Profile Info
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '${user?.username}',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'WeSell ID: ${user?.userId.substring(0, 14)}...',
+                                style: TextStyle(
+                                  color: Colors.grey[600],
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        const SizedBox(height: 4),
-                        Text(
-                          'WeSell ID: il80s-',
-                          style: TextStyle(
-                            color: Colors.grey[600],
-                            fontSize: 14,
-                          ),
+                        const SizedBox(width: 20),
+                        // QR Code Icon
+                        // Icon(
+                        //   Icons.qr_code,
+                        //   size: 24,
+                        //   color: Colors.grey[600],
+                        // ),
+                        // const SizedBox(width: 8),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 16,
+                          color: Colors.grey[400],
                         ),
                       ],
                     ),
                   ),
-                  // QR Code Icon
-                  Icon(
-                    Icons.qr_code,
-                    size: 24,
-                    color: Colors.grey[600],
+                ),
+                Divider(
+                  height: 1,
+                  thickness: 1,
+                  indent: 16,
+                  endIndent: 16,
+                  color: Colors.grey[200],
+                ),
+                ListTile(
+                  leading: const Icon(Icons.phone_iphone_rounded, color: Colors.blue),
+                  title: const Text('Phone'),
+                  // trailing: Icon(Icons.verified_rounded, size: 16, color: Colors.green),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // User name
+                      Text(
+                        '${user!.phoneNumber.substring(0, 6)}${'*' * (user.phoneNumber.length - 8)}${user.phoneNumber.substring(user.phoneNumber.length - 2)}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Icon(Icons.verified_rounded, size: 16, color: Colors.green),
+                      const SizedBox(width: 8),
+                      Icon(Icons.arrow_forward_ios_rounded, size: 16, color: Colors.grey[400]),
+                    ],
                   ),
-                  const SizedBox(width: 8),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    size: 16,
-                    color: Colors.grey[400],
-                  ),
-                ],
-              ),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                  // onTap: () {
+                  //
+                  // },
+                  tileColor: Colors.white,
+                ),
+                Divider(
+                  height: 1,
+                  thickness: 1,
+                  indent: 16,
+                  endIndent: 16,
+                  color: Colors.grey[200],
+                ),
+                // Settings Button
+                Divider(
+                  height: 1,
+                  thickness: 1,
+                  indent: 16,
+                  endIndent: 16,
+                  color: Colors.grey[200],
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings, color: Colors.blue),
+                  title: const Text('Setting'),
+                  trailing: Icon(Icons.arrow_forward_ios, size: 16, color: Colors.grey[400]),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                  onTap: () {
+                    GoRouter.of(context).push('/setting');
+                  },
+                  tileColor: Colors.white,
+                ),
+                Divider(
+                  height: 1,
+                  thickness: 1,
+                  indent: 16,
+                  endIndent: 16,
+                  color: Colors.grey[200],
+                ),
+              ],
             ),
-            // Settings Button
-            Divider(
-              height: 1,
-              thickness: 1,
-              indent: 16,
-              endIndent: 16,
-              color: Colors.grey[200],
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings, color: Colors.blue),
-              title: const Text('Setting'),
-              trailing: Icon(Icons.arrow_forward_ios, size: 16, color: Colors.grey[400]),
-              contentPadding: const EdgeInsets.symmetric(horizontal: 20),
-              onTap: () {
-                GoRouter.of(context).push('/setting');
-              },
-              tileColor: Colors.white,
-            ),
-            Divider(
-              height: 1,
-              thickness: 1,
-              indent: 16,
-              endIndent: 16,
-              color: Colors.grey[200],
-            ),
-          ],
-        ),
+          );
+        },
       ),
       bottomNavigationBar: const BottomNavBar(),
     );
